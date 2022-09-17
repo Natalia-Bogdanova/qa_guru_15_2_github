d00f4ff555d5c557a08172339e450691934e2f4c
Natalia-Bogdanova <bogdanowa.vlg@gmail.com>
2022-09-17 23:56:57 +0300

    fix logic

--- src/test/java/training/qa/ExampleTest.java
+++ src/test/java/training/qa/ExampleTest.java
@@ -7,10 +7,10 @@ public class ExampleTest {
 
     @Test
     void exampleTest1() {
-        Assertions.assertTrue(2>1);
+        Assertions.assertTrue(2 >= 1);
     }
     @Test
     void exampleTest2() {
-        Assertions.assertTrue(3>2);
+        Assertions.assertTrue(3 >= 2);
     }
 }
